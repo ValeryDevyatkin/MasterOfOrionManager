0475ab74dc777bccbc505a67a8bbab9abb34c97a
ValeraDeviatkin <valera9kin@inbox.ru>
2020-12-02 21:59:20 +0300

    ConfigurationListRegion improvement.

--- src/OrionManager/Constants/UiConstants.cs
+++ src/OrionManager/Constants/UiConstants.cs
@@ -4,17 +4,22 @@ namespace OrionManager.Constants
 {
     internal static class UiSizes
     {
+        public const double BorderSize = 2d;
+        public const double HighlightedBorderSize = 3d;
+
         public const double TextSpacing = 5d;
         public const double InnerSpacing = 15d;
         public const double OuterSpacing = 30d;
 
+        public static readonly Thickness BorderThickness = new Thickness(BorderSize);
+        public static readonly Thickness HighlightedBorderThickness = new Thickness(HighlightedBorderSize);
+
         public static readonly GridLength TextSpacingGridLength = new GridLength(TextSpacing);
         public static readonly GridLength InnerSpacingGridLength = new GridLength(InnerSpacing);
         public static readonly GridLength OuterSpacingGridLength = new GridLength(OuterSpacing);
+
+        public static readonly Thickness TextSpacingThickness = new Thickness(TextSpacing);
         public static readonly Thickness InnerSpacingThickness = new Thickness(InnerSpacing);
         public static readonly Thickness OuterSpacingThickness = new Thickness(OuterSpacing);
-
-        public static readonly Thickness OuterSpacingLeftRightThickness = new Thickness(
-            OuterSpacing, 0d, OuterSpacing, 0d);
     }
 }
---
+++ src/OrionManager/Wpf/TriggerActions/ListBoxNavigationTriggerAction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using BAJIEPA.Tools.Helpers;
+
+namespace OrionManager.Wpf.TriggerActions
+{
+    internal class ListBoxNavigationTriggerAction : TriggerAction<UIElement>
+    {
+        protected override void Invoke(object parameter)
+        {
+            if (ListBox == null)
+            {
+                throw new ArgumentNullException(nameof(ListBox));
+            }
+
+            if (ItemToNavigate == null)
+            {
+                throw new ArgumentException(nameof(ItemToNavigate));
+            }
+
+            try
+            {
+                ListBox.SelectedItem = ItemToNavigate;
+                ListBox.ScrollIntoView(ListBox.SelectedItem);
+                ListBox.Focus();
+            }
+            catch (Exception e)
+            {
+                this.LogCriticalException(e);
+            }
+        }
+
+        #region ListBox dependency: ListBox
+
+        public static readonly DependencyProperty ListBoxProperty =
+            DependencyProperty.Register(
+                nameof(ListBox),
+                typeof(ListBox),
+                typeof(ListBoxNavigationTriggerAction),
+                new PropertyMetadata(default(ListBox)));
+
+        public ListBox ListBox
+        {
+            get => (ListBox) GetValue(ListBoxProperty);
+            set => SetValue(ListBoxProperty, value);
+        }
+
+        #endregion
+
+        #region ItemToNavigate dependency: object
+
+        public static readonly DependencyProperty ItemToNavigateProperty =
+            DependencyProperty.Register(
+                nameof(ItemToNavigate),
+                typeof(object),
+                typeof(ListBoxNavigationTriggerAction),
+                new PropertyMetadata(default(object)));
+
+        public object ItemToNavigate
+        {
+            get => GetValue(ItemToNavigateProperty);
+            set => SetValue(ItemToNavigateProperty, value);
+        }
+
+        #endregion
+    }
+}
