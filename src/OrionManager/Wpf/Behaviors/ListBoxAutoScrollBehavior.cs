75c21b8b9a4aa6fa28aa24cdf13607615550a4a4
ValeraDeviatkin <valera9kin@inbox.ru>
2020-12-03 18:31:45 +0300

    ListBoxAutoScrollBehavior added.

---
+++ src/OrionManager/Wpf/Behaviors/ListBoxAutoScrollBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace OrionManager.Wpf.Behaviors
+{
+    internal class ListBoxAutoScrollBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.SelectionChanged += AssociatedObjectOnSelectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.SelectionChanged -= AssociatedObjectOnSelectionChanged;
+        }
+
+        private void AssociatedObjectOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AssociatedObject.ScrollIntoView(AssociatedObject.SelectedItem);
+        }
+    }
+}
