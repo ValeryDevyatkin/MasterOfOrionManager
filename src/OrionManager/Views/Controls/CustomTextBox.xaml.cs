6167a3c9bfe808c8f1df1f23c6fe6b4372eb3b58
ValeraDeviatkin <valera9kin@inbox.ru>
2020-12-03 20:30:40 +0300

    CustomTextBox added.

---
+++ src/OrionManager/Views/Controls/CustomTextBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace OrionManager.Views.Controls
+{
+    internal partial class CustomTextBox
+    {
+        public CustomTextBox()
+        {
+            InitializeComponent();
+        }
+
+        #region Label dependency: string
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(CustomTextBox),
+                new PropertyMetadata(default(string)));
+
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        #endregion
+    }
+}
