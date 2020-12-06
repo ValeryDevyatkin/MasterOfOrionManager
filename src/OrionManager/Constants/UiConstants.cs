65efa909ca11c4ba86a917d2f653e506f2b759fe
ValeraDeviatkin <valera9kin@inbox.ru>
2020-12-06 17:04:51 +0300

    CustomComboBox created.

--- src/OrionManager/Constants/UiConstants.cs
+++ src/OrionManager/Constants/UiConstants.cs
@@ -7,6 +7,7 @@ namespace OrionManager.Constants
         public const double BorderRadiusSize = 5d;
         public const double BorderThicknessSize = 2d;
         public const double HighlightedBorderThicknessSize = 3d;
+        public const double AlertBorderThicknessSize = 1d;
 
         public const double TextSpacing = 5d;
         public const double InnerSpacing = 15d;
@@ -15,6 +16,7 @@ namespace OrionManager.Constants
         public static readonly CornerRadius BorderRadius = new CornerRadius(BorderRadiusSize);
         public static readonly Thickness BorderThickness = new Thickness(BorderThicknessSize);
         public static readonly Thickness HighlightedBorderThickness = new Thickness(HighlightedBorderThicknessSize);
+        public static readonly Thickness AlertBorderThickness = new Thickness(AlertBorderThicknessSize);
 
         public static readonly GridLength TextSpacingGridLength = new GridLength(TextSpacing);
         public static readonly GridLength InnerSpacingGridLength = new GridLength(InnerSpacing);
---
+++ src/OrionManager/Views/Controls/CustomComboBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace OrionManager.Views.Controls
+{
+    public partial class CustomComboBox
+    {
+        public CustomComboBox()
+        {
+            InitializeComponent();
+        }
+
+        #region Label dependency: string
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(CustomComboBox),
+                new PropertyMetadata(default(string)));
+
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        #endregion
+    }
+}
