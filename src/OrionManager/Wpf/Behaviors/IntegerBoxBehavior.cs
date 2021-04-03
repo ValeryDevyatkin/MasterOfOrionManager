a14cb6ed9148d8e7ecf906a68cc11693cd739dff
ValeryDeviatkin <valera9kin@inbox.ru>
2021-04-04 00:56:22 +0300

    IntegerBoxBehavior created.

---
+++ src/OrionManager/Wpf/Behaviors/IntegerBoxBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace OrionManager.Wpf.Behaviors
+{
+    internal class IntegerBoxBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewKeyDown += AssociatedObjectOnPreviewKeyDown;
+            AssociatedObject.LostFocus += AssociatedObjectOnLostFocus;
+            AssociatedObject.TextChanged += AssociatedObjectOnTextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.PreviewKeyDown -= AssociatedObjectOnPreviewKeyDown;
+            AssociatedObject.LostFocus -= AssociatedObjectOnLostFocus;
+            AssociatedObject.TextChanged -= AssociatedObjectOnTextChanged;
+        }
+
+        private void AssociatedObjectOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(AssociatedObject.Text) &&
+                AssociatedObject.Text != "-")
+            {
+                UpdateBinding();
+            }
+        }
+
+        private void AssociatedObjectOnLostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateBinding();
+        }
+
+        private void AssociatedObjectOnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                UpdateBinding();
+            }
+
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void UpdateBinding()
+        {
+            AssociatedObject.TextChanged -= AssociatedObjectOnTextChanged;
+            AssociatedObject.GetBindingExpression(TextBox.TextProperty)?.UpdateTarget();
+            AssociatedObject.TextChanged += AssociatedObjectOnTextChanged;
+        }
+    }
+}
