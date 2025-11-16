f1390ae28f11b533e71544a9ca56bef593b69c5f
Valery Dzeviatkin <valera9kin@inbox.ru>
2025-11-16 20:02:22 +0300

    Viewbox adjustments

--- src/OrionManager/Theme/UiSizes.cs
+++ src/OrionManager/Theme/UiSizes.cs
@@ -7,11 +7,13 @@ namespace OrionManager.Theme
         #region Border
 
         public const double BorderRadiusSize = 5d;
+        public const double SmallerBorderRadiusSize = 2d;
         public const double MinimalBorderThicknessSize = 1d;
         public const double BorderThicknessSize = 2d;
         public const double HighlightedBorderThicknessSize = 3d;
 
         public static readonly CornerRadius BorderRadius = new CornerRadius(BorderRadiusSize);
+        public static readonly CornerRadius SmallerBorderRadius = new CornerRadius(SmallerBorderRadiusSize);
         public static readonly Thickness MinimalBorderThickness = new Thickness(MinimalBorderThicknessSize);
         public static readonly Thickness BorderThickness = new Thickness(BorderThicknessSize);
         public static readonly Thickness HighlightedBorderThickness = new Thickness(HighlightedBorderThicknessSize);
@@ -31,6 +33,7 @@ namespace OrionManager.Theme
         public static readonly Thickness TextSpacingThickness = new Thickness(TextSpacing);
         public static readonly Thickness InnerSpacingThickness = new Thickness(InnerSpacing);
         public static readonly Thickness OuterSpacingThickness = new Thickness(OuterSpacing);
+        public static readonly Thickness OuterSpacingLeftNegativeThickness = new Thickness(- OuterSpacing, 0, 0, 0);
 
         #endregion
     }
